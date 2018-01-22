5a4cbafee866c76203f48a84665445f8ed367b6c
caglarpanus <panuscaglar@gmail.com>
2018-01-22 12:13:30 -0500

    First Version of the Layout

---
+++ assets/javascript/app.js
@@ -0,0 +1,11 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyB968gjyX338rGHvY19E_HgI4AKEJzSaUc",
+    authDomain: "rps-game-a58e6.firebaseapp.com",
+    databaseURL: "https://rps-game-a58e6.firebaseio.com",
+    projectId: "rps-game-a58e6",
+    storageBucket: "rps-game-a58e6.appspot.com",
+    messagingSenderId: "39865592635"
+  };
+  
+  firebase.initializeApp(config);
